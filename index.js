d7dab659c5494d930f840c455900f53ce75b9585
Steven Prins <steven@kaartje2go.nl>
2019-03-29 19:22:19 +0100

    Init

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require('express')
+const Tesseract = require('tesseract.js')
+const request = require('request');
+const multer = require('multer')
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
+
+const app = express()
+
+const port = process.env.PORT || 3000;
+
+const createResponse = (buffer, lang, callback) => {
+  Tesseract.recognize(buffer, {
+    lang: lang
+  }).then((result) => {
+    return callback({
+      text: result.text,
+    })
+  })
+}
+
+app.get('/recognize', (req, res) => {
+  const url = req.query.url;
+  const langauge = req.query.lang || 'eng';
+  if (!url) {
+    return res.send({
+      error: 'URL parameter missing'
+    })
+  }
+
+  request.head(url, (err, response, body) => {
+    console.log('content-type:', response.headers['content-type']);
+    console.log('content-length:', response.headers['content-length']);
+
+    request.get({
+      url: url,
+      encoding: null
+     }, (err, response, body) => {
+      createResponse(body, langauge, (result) => {
+        return res.send(result.text.replace(/(?:\r\n|\r|\n)/g, '<br>'))
+      })
+    })
+  });
+
+})
+
+app.post('/recognize', upload.single('image'), (req, res) => {
+  createResponse(req.file.buffer, 'en', (result) => {
+    return res.send(result.text.replace(/(?:\r\n|\r|\n)/g, '<br>'))
+  })
+})
+
+app.get('/', (req, res) => {
+  res.send(`
+  <form action="/recognize" method="post" enctype="multipart/form-data">
+  Select image to upload:
+  <input type="file" name="image" id="image">
+  <input type="submit" value="Upload Image" name="submit">
+</form>
+  `)
+})
+
+app.listen(port, () => console.log(`image-to-text-api listening on port ${port}!`))
